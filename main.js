c903347bcffe1e236d964d08f308fdd1c584a5d4
Tom Weatherhead <tom_weatherhead@hotmail.com>
2015-12-16 18:36:27 -0500

    Fixed the attribution; added metadata to package.json.

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-// See https://medium.com/developers-writing/building-a-desktop-application-with-electron-204203eeb658#.rpbzyljfo
-
-// Once package.json is in place (possibly without any devDependencies), run: npm install --save-dev electron-prebuilt
-
 'use strict';
 
 var app = require('app');
