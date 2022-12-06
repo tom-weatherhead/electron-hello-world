43800defd3353601f077280da8a12ad481c1d97a
Tom Weatherhead <tomw@2hrd4u.org>
2022-12-05 20:12:13 -0700

    Dev

--- main.js
+++ main.js
@@ -1,17 +1,88 @@
 'use strict';
 
-const { app, BrowserWindow, ipcMain, screen } = require('electron');
+const { app, BrowserWindow, ipcMain, Menu, screen, Tray } = require('electron');
 const path = require('path');
 
+// const platform = os.platform();
+const platform = process.platform;
+// const isPlatformWindows = platform === 'win32';
+const isPlatformMac = platform === 'darwin';
+// const isPlatformLinux = platform === 'linux';
+
+// console.log('platform is', platform);
+
+const faviconFilePath = path.join(__dirname, 'src', 'assets', 'icons', 'png', 'favicon.png');
+const trayIconFilePath = faviconFilePath;
+
+// const trayIconFilePath = path.join(__dirname, 'src', 'assets', 'icons', 'macos-tray', 'nipple-icon01-024x024.png');
+
+const appIconFilePath = path.join(__dirname, 'src', 'assets', 'icons', 'macos-dock', 'green-f-01-96x96.png');;
+// const pngIconFilePath = appIconFilePath;
+
 let mainWindow = undefined;
+
+// See https://electronjs.org/docs/api/app#appdisablehardwareacceleration
+// app.disableHardwareAcceleration();
+
+// TODO: Use app.isUnityRunning() on Linux?
+
+if (isPlatformMac) {
+	app.setAboutPanelOptions({
+		applicationName: 'Foo',
+		applicationVersion: '0.0.0',
+		copyright: 'Copyright (c) 2018-2022 Buckwheat Unlimited',
+		version: '0.0.0', // macOS only.
+		credits: 'Praise the LORD!' // macOS and Windows only.
+		// , authors: ['Buckwheat'], // Linux only.
+		// website: 'https://2hrd4u.org' // , // Linux only.
+		// iconPath: pngIconFilePath // Linux and Windows only.
+	});
+}
+
+function setDockMenu() {
+
+	if (!isPlatformMac) {
+		return;
+	}
+
+	const dockMenu = Menu.buildFromTemplate([
+		{
+			label: 'New Window',
+			click() {
+				console.log('New Window');
+			}
+		},
+		{
+			label: 'New Window with Settings',
+			submenu: [
+				{
+					label: 'Basic'
+				},
+				{
+					label: 'Pro'
+				}
+			]
+		},
+		{
+			label: 'New Foo Command...'
+		}
+	]);
+
+	app.dock.setMenu(dockMenu);
+
+	// getPathToRandomAppIconPNGFile(appIconPNGFilesDir).then((appIconPNGFile) => {
+	// 	console.log('Using app icon at', appIconPNGFile);
+	// 	pngIconFilePath = appIconPNGFile;
+	// 	app.dock.setIcon(appIconPNGFile);
+	// });
+	app.dock.setIcon(appIconFilePath);
+}
 
 function isMainDisplay(display) {
     return display.bounds.x === 0 && display.bounds.y === 0;
 }
 
 function createWindow() {
-    // const { screen } = require('electron');
-
     const displays = screen.getAllDisplays();
 
     // Panasonic TV: id == 1, bounds.x == 0
@@ -66,6 +137,10 @@ function createWindow() {
             // width: 1024,
             // height: 768
 
+			backgroundColor: '#ffffff',
+			// backgroundColor: '#7851a9', // From the TouchBar button
+			// icon: 'assets/favicon.png', // On Windows, use an .ico file for best results.
+			// icon: faviconFilePath, // ThAW: Does this have any effect? I.e. On non-Mac?
             title: `Electron BrowserWindow on Display ${display.id}`,
             x,
             y,
@@ -96,8 +171,10 @@ function createWindow() {
 app.whenReady().then(() => {
     console.log('App event: ready');
 
+	// Create the first window
 	createWindow()
 
+	// Ensure that there is always at least one window.
 	app.on('activate', () => {
 
 		if (BrowserWindow.getAllWindows().length === 0) {
@@ -105,12 +182,28 @@ app.whenReady().then(() => {
 		}
 	});
 
+	// Create the tray icon
+	const tray = new Tray(trayIconFilePath); // Should the icon be 24x24 ?
+
+	// Configure the application's icon in the tray
+
+	tray.on('click', () => {
+		// When the app's tray icon is clicked, do some appropriate action.
+		console.log('Tray event: click');
+
+		// win.isVisible() ? win.hide() : win.show();
+	});
+
+	// Create all IPC (inter-process communications) listeners.
 	ipcMain.on('set-title', (event, title) => {
 		const webContents = event.sender;
 		const win = BrowserWindow.fromWebContents(webContents);
 
 		win.setTitle(title);
 	});
+
+	// On macOS, set up the dock menu
+	setDockMenu();
 });
 
 // Alternative to window.on('focus', ...); :
